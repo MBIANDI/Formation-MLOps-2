220831c052c2000ff6a27259f0d1da97c620fb20
MBIANDI <mbialaura12@gmail.com>
2024-10-25 08:59:18 +0000

    predict

--- exposition/model_as_a_service/inference.py
+++ exposition/model_as_a_service/inference.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify
+from formation_indus_ds_avancee.feature_engineering import prepare_features
+from formation_indus_ds_avancee.train_and_predict import predict
+from config import MODEL_PATH, INFERENCE_DATA
 
 app = Flask(__name__)
 
@@ -12,4 +15,8 @@ def health():
 
 @app.route('/predict')
 def predict_endpoint():
-    raise NotImplementedError
+    
+    features = prepare_features(data=INFERENCE_DATA, training_mode=False)
+    features = predict(features, model_path= MODEL_PATH)
+
+    return jsonify(features['predictions'])
